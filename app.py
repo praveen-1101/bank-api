f0702835b6a2da1e2868431872b629b1ce58b9d8
praveen-1101 <borapraveen1101@gmail.com>
2025-01-25 17:28:12 +0530

    all done

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Sample Data
+# Main Banks Data
 banks = [
     {"id": 1, "name": "STATE BANK OF INDIA"},
     {"id": 2, "name": "PUNJAB NATIONAL BANK"},
     {"id": 3, "name": "CANARA BANK"}
 ]
 
+# Branches data 
 branches = [
    
     {"branch_code": "ABHY0065001", "bank_id": 1, "branch": "RTGS-HO", "ifsc": "SBIN0001", "address": "ABHYUDAYA BANK BLDG., B.NO.71, NEHRU NAGAR, KURLA (E), MUMBAI-400024", "city": "MUMBAI", "district": "GREATER MUMBAI", "state": "MAHARASHTRA"},
@@ -18,21 +19,23 @@ branches = [
     {"branch_code": "CAN001", "bank_id": 3, "branch": "CHENNAI BRANCH", "ifsc": "CAN0001", "address": "R.A. PURAM, CHENNAI, TAMIL NADU - 600028", "city": "CHENNAI", "district": "CHENNAI", "state": "TAMIL NADU"},
     {"branch_code": "CAN002", "bank_id": 3, "branch": "KOCHI BRANCH", "ifsc": "CAN0002", "address": "MG ROAD, KOCHI, KERALA - 682035", "city": "KOCHI", "district": "ERNAKULAM", "state": "KERALA"}
 ]
+
 @app.route('/')
 def home():
-    return "Welcome to the Bank API"
+    return "Hello, everyone!, Welcome to the Bank API"
 
-# Endpoint to get all banks
+# An endpoint to get all the banks
 @app.route('/banks', methods=['GET'])
 def get_banks():
     return jsonify({"banks": banks})
 
-# Endpoint to get branches for a specific bank
+# An endpoint to get branches of all the banks
 @app.route('/banks/<int:bank_id>/branches', methods=['GET'])
 def get_branches_for_bank(bank_id):
     bank_branches = [branch for branch in branches if branch["bank_id"] == bank_id]
     return jsonify({"branches": bank_branches})
 
+# An endpoint for non existing banks or branch ids
 @app.errorhandler(404)
 def page_not_found(error):
     response = {
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,22 +2,25 @@ import unittest
 from app import app
 
 class TestBankAPI(unittest.TestCase):
-
-    @classmethod
+    #To define a class method
+    @classmethod               
     def setUpClass(cls):
         cls.client = app.test_client()
 
+    #for checking the response of banks endpoint
     def test_get_banks(self):
         response = self.client.get('/banks')
         self.assertEqual(response.status_code, 200)
         self.assertIn('banks', response.json)
 
+    #for checking the response of branches endpoint
     def test_get_branches_for_bank(self):
         response = self.client.get('/banks/1/branches')
         self.assertEqual(response.status_code, 200)
         self.assertIn('branches', response.json)
         self.assertEqual(response.json['branches'][0]['bank_id'], 1)
 
+    #for checking the response of endpoint for a non-existing bank
     def test_get_branches_for_non_existing_bank(self):
         response = self.client.get('/banks/999/branches')  # Non-existing bank ID
         self.assertEqual(response.status_code, 404)
@@ -25,5 +28,6 @@ class TestBankAPI(unittest.TestCase):
         self.assertEqual(response.json['message'], 'The requested URL is not present at the moment. Please check the endpoint and try once again.')
 
 
+#main function
 if __name__ == '__main__':
     unittest.main()
